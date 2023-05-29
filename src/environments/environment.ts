a500ec3e4f677e6513630170f58eb89a98e7d5bd
Andrei Husovschi <husovschi@protonmail.com>
2023-05-29 19:25:33 +0300

    Using husovschi cloud endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  backendUrl: 'http://husovschi-app.duckdns.org:8000',
+  backendUrl: 'http://husovschi-cloud.duckdns.org:8000',
   envVarKeys: [
     { key: 'MY_NODE_NAME', value: '<MY_NODE_NAME>' },
     { key: 'MY_POD_NAME', value: '<MY_POD_NAME>' },
